9661a9a835a5c352bf4c2166ae528d03676820e1
lan496 <kohei19950508@gmail.com>
2017-11-15 02:14:14 +0900

    unpack by numpy

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -1,5 +1,6 @@
 import argparse
-import networkx as nx
+# import networkx as nx
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -12,33 +13,20 @@ if __name__ == "__main__":
     # read input
     with open(args.input, "r") as f:
         V, E = [int(x) for x in next(f).split()]
-        u = []
-        v = []
-        w = []
-        for i in range(E):
-            uu, vv, ww = [int(x) for x in next(f).split()]
-            uu -= 1
-            vv -= 1
-            u.append(uu)
-            v.append(vv)
-            w.append(ww)
+        u, v, w = np.array([[int(x) for x in next(f).split()]
+                            for i in range(E)]).T
+        u -= 1
+        v -= 1
+
         Vemb, Eemb = [int(x) for x in next(f).split()]
-        a = []
-        b = []
-        for i in range(E):
-            aa, bb = [int(x) for x in next(f).split()]
-            aa -= 1
-            bb -= 1
-            a.append(aa)
-            b.append(bb)
+        a, b = np.array([[int(x) for x in next(f).split()]
+                         for i in range(E)]).T
+        a -= 1
+        b -= 1
 
     # read output
     with open(args.output, "r") as f:
-        s = []
-        t = []
-        for i in range(V):
-            ss, tt = [int(x) for x in next(f).split()]
-            ss -= 1
-            tt -= 1
-            s.append(ss)
-            t.append(tt)
+        s, t = np.array([[int(x) for x in next(f).split()]
+                         for i in range(V)]).T
+        s -= 1
+        t -= 1
